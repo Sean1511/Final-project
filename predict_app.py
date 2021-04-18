650990ccdc3c982e47adf23b5c201a2bbbb06f80
sean1511 <sean1511@gmail.com>
2021-04-18 16:58:19 +0300

    18/04 work

--- predict_app.py
+++ predict_app.py
@@ -13,7 +13,8 @@ app = Flask(__name__)
 
 FEMALE = 0
 MALE = 1
-model_name = 'xception_segmentation_split_40ep.h5'
+current_model = 'Xception'
+model = load_model('models/xception_segmentation_split_40ep.h5')
 preprocess_func = tf.keras.applications.xception.preprocess_input
 scale = (299, 299)
 
@@ -42,7 +43,6 @@ def image_segment(image):
                 images.append(div_im)
             x = x + 200
         y = y + 200
-
     return images
 
 
@@ -57,16 +57,37 @@ def rand_patch(image):
         im = image[rany:rany+400, ranx:ranx+400].copy()
         if count_not_white_pixels(im, 4500):
             patch = im
-
     return patch
 
 
-def predict_image(image, model):
+def select_model(name):
+    if name == "Xception":
+        model = load_model('models/xception_segmentation_split_40ep.h5')
+        preprocess_func = tf.keras.applications.xception.preprocess_input
+        scale = (299, 299)
+    elif name == "VGG16":
+        model = load_model('models/vgg16_gender_split_segmetation_19ep.h5')
+        preprocess_func = tf.keras.applications.vgg16.preprocess_input
+        scale = (224, 224)
+    elif name == "Efficienet":
+        model = load_model('models/efficientnet_segmentation_split_15ep.h5')
+        preprocess_func = tf.keras.applications.efficientnet.preprocess_input
+        scale = (224, 224)
+    elif name == "Nasnet":
+        model = load_model('models/NasNet_segmentation_split_20ep.h5')
+        preprocess_func = tf.keras.applications.nasnet.preprocess_input
+        scale = (331, 331)
+    return model, preprocess_func, scale
+
+
+def load_default_settings():
+    return current_model, model, preprocess_func, scale
+
+def predict_image(image, model, preprocess_func, scale):
     patches = image_segment(image)   # extract patches from image
     if len(patches) % 2 == 0 : patches.append(rand_patch(image)) # if number of patches is even, add 1 random patch to make it odd
     x_test = []
     for patch in patches:   # Process each patch to fit the model
-        # patch = cv2.cvtColor(patch, cv2.COLOR_GRAY2BGR)
         patch = cv2.resize(patch, scale)
         patch = preprocess_func(patch)
         x_test.append(patch)
@@ -86,27 +107,31 @@ def predict_image(image, model):
     print('female score : ', f_score/patches_count)
     print('classification : {0}'.format('MALE' if m_score > f_score else 'FEMALE'))
     print()
-    return m_score/patches_count, f_score/patches_count
+    return m_score/patches_count, f_score/patches_count, patches_count, ('classification : {0}'.format('MALE' if m_score > f_score else 'FEMALE'))
 
-
-model = load_model(model_name)
-print(" * Model loaded!")
 
 @app.route("/predict", methods=["POST"])
 def predict():
     message = request.get_json(force=True) # Get image from user
     encoded = message['image']
+    model_name = message['model']
+    current_model, model, preprocess_func, scale = load_default_settings()
+    if (model_name != current_model):
+        model, preprocess_func, scale = select_model(model_name)
+        current_model = model_name
     decoded = base64.b64decode(encoded) # decode data
     image_stream = io.BytesIO(decoded)
     image_stream.seek(0)
     file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
     image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-    prediction = predict_image(image, model)
+    prediction = predict_image(image, model, preprocess_func, scale)
 
     response = {
         'prediction': {
             'Male': prediction[0],
-            'Female': prediction[1]
+            'Female': prediction[1], 
+            'patches_count':prediction[2],
+            'classification':prediction[3]
         }
     }
 
